877732130dbf5c71d253267ebbdb59d5127687e6
4212ganteng <azizmuslim051@gmail.com>
2023-12-04 11:13:53 +0700

    prisma orm

--- src/app/api/notes/route.js
+++ src/app/api/notes/route.js
@@ -1,12 +1,42 @@
+import { prisma } from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ message: "hello next-backend" }, { status: 200 });
+  try {
+    // prisma.model.method
+    const allNotes = await prisma.notes.findMany();
+    return NextResponse.json(
+      { data: allNotes, message: "fetch data success!" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error, meesage: "fetch data error!" },
+      { status: 500 }
+    );
+  }
 }
-export async function PATCH() {
-  return NextResponse.json({ message: "hello next-backend" }, { status: 200 });
-}
+
 export async function POST(req) {
-  const { name } = await req.json();
-  return NextResponse.json({ message: `hello , ${name}` }, { status: 200 });
+  const { content, user, title } = await req.json();
+
+  try {
+    const createNote = await prisma.notes.create({
+      data: {
+        title,
+        content,
+        user,
+      },
+    });
+
+    return NextResponse.json(
+      { data: createNote, message: "success create note" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error, message: "Failed to create note" },
+      { status: 500 }
+    );
+  }
 }
